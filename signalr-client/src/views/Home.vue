d69c486ad6e6259eb069b467fed7a9d2d1e3ce9f
Kazuyuki Miyake <miyake@zenarc.jp>
2019-05-26 08:46:42 +0900

    Added Element

--- signalr-client/src/views/Home.vue
+++ signalr-client/src/views/Home.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="home">
-    <ul>
-      <li
-        v-for="tweet in tweets"
-        v-bind:key="tweet.id"
-      >
-        {{ tweet.tweetText }}
-      </li>
-    </ul>
+    <el-container>
+      <el-header>
+        <span class="title">リアルタイムツイートモニター </span>
+        <small>"de:code" "decode" "#decode19" で検索したツイート</small>
+      </el-header>
+      <el-main>
+        <div class="block">
+          <el-timeline>
+            <el-timeline-item
+              v-for="tweet in tweets"
+              v-bind:key="tweet.id"
+              :timestamp="tweet.timeStamp"
+              placement="top"
+            >
+              <el-card>
+                <h4>{{ tweet.name }}</h4>
+                <p>{{ tweet.tweetText }}</p>
+              </el-card>
+            </el-timeline-item>
+          </el-timeline>
+        </div>
+      </el-main>
+    </el-container>
   </div>
 </template>
+
+<style scoped>
+.home {
+  text-align: left;
+}
+.title {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+</style>
+
 
 <script lang="ts">
 import Vue from "vue";
@@ -20,7 +46,9 @@ import {
   JsonHubProtocol
 } from "@aspnet/signalr";
 import { constants } from "crypto";
+import { DateType } from "element-ui/types/calendar";
 
+// API ペイロード
 class Tweet {
   id: string = "";
   tweetText: string = "";
@@ -28,8 +56,10 @@ class Tweet {
   name: string = "";
   userId: string = "";
   _ts: number = 0;
+  timeStamp: string = "";
 }
 
+// ビューモデル
 export default Vue.extend({
   name: "home",
   data() {
@@ -42,6 +72,7 @@ export default Vue.extend({
   methods: {
     displayTweet(messages: Tweet[]): void {
       messages.forEach((item: Tweet) => {
+        item.timeStamp = new Date(item._ts * 1000).toLocaleTimeString();
         this.tweets.push(item);
         console.log(item);
       });
@@ -64,7 +95,7 @@ export default Vue.extend({
       .catch(console.error);
 
     // SignalRからの呼び出し
-    connection.on("newMessage", (tweets: any) => this.displayTweet(tweets));
+    connection.on("newMessage", (tweets: Tweet[]) => this.displayTweet(tweets));
 
     // 切断
     connection.onclose(() => console.log("disconnected"));
